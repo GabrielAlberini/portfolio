f0b29fe09de4ac870116fee948c4dad9cc56acee
CuerpoCasa <gabialberini733@gmail.com>
2021-12-05 18:35:51 -0300

    V1.6 Popup Skills, I will fix it

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 /*Menu*/
 ((d) => {
-    const $btnMenu = d.querySelector(".menu-btn"),
-    $menu = d.querySelector(".menu");
+    const NAME_SKILLS = {
+        html: "HTML"
+    }
 
-    $btnMenu.addEventListener("click", e=> {
+    const $btnMenu = d.querySelector(".menu-btn"),
+    $menu = d.querySelector(".menu"),
+    $skills = d.querySelector(".skills"),
+    $tool = d.querySelector(".tool"),
+    $nameTool = $skills.getElementsByTagName("img");
+    console.log($nameTool[0].id)
+
+
+    $btnMenu.addEventListener("click", e => {
         $btnMenu.firstElementChild.classList.toggle("none");
         $btnMenu.lastElementChild.classList.toggle("none");
-        $menu.classList.toggle("is-active")
+        $menu.classList.toggle("is-active");
     });
-    d.addEventListener("click", e=> {
+
+    d.addEventListener("click", e => {
         if(!e.target.matches(".menu a")) return false;
         $btnMenu.firstElementChild.classList.remove("none");
         $btnMenu.lastElementChild.classList.add("none");
         $menu.classList.remove("is-active")
     });
+
+    for (tool of $nameTool) {
+        $tool.classList.toggle("is-active")
+    }
+
 })(document);
