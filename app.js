a57e32ae3b1ca3ae2ad47ca3fdc2114fe61a108d
CuerpoCasa <gabialberini733@gmail.com>
2021-12-17 17:52:53 -0300

    V1.9

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /*Menu*/
 ((d) => {
-
+    //Se declaran anteponiendo el $ porque 
+    //están haciendo referencia a un elemento del DOM
     const $btnMenu = d.querySelector(".menu-btn"),
     $menu = d.querySelector(".menu")
 
@@ -17,4 +18,44 @@
         $menu.classList.remove("is-active")
     });
 
+
+
 })(document);
+
+/*ContactForm*/
+
+    //Se declaran anteponiendo el $ porque 
+    //están haciendo referencia a un elemento del DOM
+    
+
+((d) => {
+    //Se declaran anteponiendo el $ porque 
+    //están haciendo referencia a un elemento del DOM
+    const $form = d.querySelector(".contact-form"),
+        $loader = d.querySelector(".contact-form-loader"),
+        $response = d.querySelector(".contact-form-response");
+    
+        $form.addEventListener("submit", e=> {
+            e.preventDefault();
+            $loader.classList.remove("none");
+            fetch("https://formsubmit.co/ajax/gabialberini733@gmail.com", {
+                method: "POST",
+                body: new FormData(e.target)
+            }).then((res)=> (res.ok ? res.json() : Promise.reject(res)))
+            .then(json =>{
+                console.log(json)
+                location.hash = "#gracias"
+                $form.reset();
+            })
+            .catch(err => {
+                console.log(err)
+                let message = err.statusText || "Ocurrió un error al enviar, intenta nuevamente"
+                $response.querySelector("h3").innerHTML = `Error ${err.status}: ${message}`
+            }).finally(()=> {
+                $loader.classList.add("none")
+                setTimeout(() => {
+                    location.hash = "#close"
+                }, 1500)
+            });   
+        });
+})(document)
